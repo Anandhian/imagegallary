a96a32e590444eb2bc68129b485a26aeee8c1b30
Anandhian <anandhianandhan29>
2024-06-10 17:51:37 +0530

    updated the all files

---
+++ src/images.js
@@ -0,0 +1,58 @@
+import "./style.css"
+
+const IMAGES=[{
+    Id:1,
+    image:'images/two.jpg',
+    p:'very deliceous icecream'
+
+},
+
+{
+    Id:1,
+     image:'images/three.jpg',
+     p:' my favorite ice'
+
+},
+{
+    Id:1,
+     image:'images/four.jpg',
+     p:'my cub'
+
+},
+{
+    Id:1,
+     image:'images/three.jpg',
+     p:'wonder icecream '
+
+},
+
+{
+    Id:1,
+     image:'images/teeee.jpg',
+     p:'cone icecream'
+
+},
+
+
+{
+    Id:1,
+     image:'images/four.jpg',
+     p:'conecub'
+        
+  
+
+},{
+    Id:1,
+     image:'images/ten.jpg',
+        p:'blacky icecream'
+
+}
+,{
+    Id:1,
+     image:'images/one.jpg',
+     p:' pink icecream'
+
+}
+
+]
+export default IMAGES
